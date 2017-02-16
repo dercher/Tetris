e642783898b8cebbef3cbdc1beae39b4b36708a2
Iurii Voitenko <jurgeban2@yandex.ua>
2017-02-16 14:34:16 +0200

    first field added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 // document.createElement();getElementById();getElementsByTagName();getElementsByName();getElementsByClassName();querySelector();querySelectorAll();el.classList.add();el.innerHTML = "";e.style.background = '';el.appendChild();setInterval();setTimeout();clearInterval();
+var matrix = [10, 20];
+var all = matrix[0] * matrix[1];
+getMatrix(20,10)
+
+function getMatrix(num1, num2) {
+	var el = document.querySelector('.main');
+	var count =0;
+  for (var i = 0; i < num1; i++) {
+  	for (var y = 0; y < num2; y++) {
+  	  el.innerHTML += '<div class="sell">' +(++count)+ '</div>'; 
+  	}
+  	  el.innerHTML += '<br class="clearfix">';
+  }
+}
+var el = document.querySelectorAll('.sell')[0];
+ var computedStyle = getComputedStyle(el); 
+        var width = computedStyle.width;
+        var height = computedStyle.height;
+  console.log('width', width, '. height', height);
